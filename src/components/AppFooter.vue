13770e383edade6e6b5904186d52c7cdece43912
Allan Pereira <149106546+CodeByAllan@users.noreply.github.com>
2025-04-19 21:53:28 -0300

    feat(ui): add responsive footer that scrolls with the page (#6)
    *Created the AppFooter component with custom text.
    *Integrated AppFooter into the main layout.
    *Adjusted footer behavior to scroll with the page content.
    *Used Quasar classes like `q-footer`, `q-pa-sm`, and `text-center` for proper responsiveness and alignment.

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,21 @@
+<template>
+  <q-footer class="text-white text-center q-pa-md">
+    <div class="footer-text text-wrap">© {{ year }} {{ label }}. All rights reserved.</div>
+  </q-footer>
+</template>
+<script setup lang="ts">
+defineProps<{
+  label?: string;
+  year?: string;
+}>();
+</script>
+<style scoped lang="scss">
+.q-footer {
+  background-color: $color-primary;
+  .footer-text {
+    font-family: $font-secondary;
+    font-size: 14px;
+    color: $color-secondary;
+  }
+}
+</style>
